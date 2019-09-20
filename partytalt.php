e5dc948a817144de7e0059b43315bb1c13d38656
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-09-20 10:54:37 +0200

    Fixade upp programmet så att det är fint

--- partytalt.php
+++ partytalt.php
@@ -2,31 +2,29 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Git out of here</title>
+    <title>Are these fruits?</title>
     <meta name="description" content="The purpose of this page is to show if a banana or a apple is a fruit!">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <img src="bilder/espresso.jpg">
     <?php
-    try {
+	
         $banan_ar_frukt = true;
         $apple_ar_frukt = true;
         if ($banan_ar_frukt && $apple_ar_frukt)
               header("location: https://www.lexico.com/en/explore/is-a-banana-a-fruit-or-a-herb");
-
-    } catch (Exception $e) {
-
-        echo 'Caught exception: ', $e->getMessage(), "\n";
-    }
+		  
     ?>
 </body>
 </html>
 <?php
-error_log('nu har vi kört denna X antal gånger', 3, 'test.log');
-/*
-  Git out of here!
-  Syftet med denna hemsida är att visa om en banan eller äpple är en frukt
-  (Kan det bli bättre än ovan????)
- */
+try{
+	
+	error_log('nu har vi kört denna X antal gånger', 3, 'test.log');
+
+}catch(Exception $e){
+	
+	echo 'Caught exception: ', $e->getMessage(), "\n";
+	
+}
 ?>
