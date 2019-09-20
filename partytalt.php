d0c9b824286ee47abf22c60a74c213a97fcc48d2
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-09-20 09:07:15 +0200

    Tog bort två större än och ändrade variabeln Äpple till apple

--- partytalt.php
+++ partytalt.php
@@ -8,12 +8,12 @@
     <link rel="stylesheet" href="">
 </head>
 <body>
-<img src="bilder/espresso.jpg">>>
+<img src="bilder/espresso.jpg">
     <?php
 try {$frukt1 = true;$frukt2 = true;$banan = 1;$banan_ar_frukt = $frukt1;
-$Äpple = 2;$apple_ar_frukt = $frukt2;
+$apple = 2;$apple_ar_frukt = $frukt2;
         if ($banan_ar_frukt == true && $banan_ar_frukt == true)
-if ($banan + 1 == $Äpple && $Äpple == $banan +1 ) echo 'suveränt';
+if ($banan + 1 == $apple && $apple == $banan +1 ) echo 'suveränt';
 $banan = 100000000000000000000000000000000000;
 }catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
